4f9539894f2b166c98d337bfcc679c53c179ed89
1ShoukR <rahminshoukoohi@gmail.com>
2025-06-20 18:24:58 -0400

    added Chi framework

---
+++ cmd/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	r := chi.NewRouter()
+
+	// Logger middleware
+	r.Use(middleware.Logger)
+
+	// Health route
+	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("OK"))
+	})
+
+	// Start server
+	fmt.Println("Server starting on :8080")
+	http.ListenAndServe(":8080", r)
+}
