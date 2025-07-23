cc869520df6210a412e2d0873113870a9733e1a9
1ShoukR <rahminshoukoohi@gmail.com>
2025-07-22 23:42:00 -0400

    feat: moved Profile model to user file; added Authlevel to User model

--- internal/models/user.go
+++ internal/models/user.go
@@ -12,23 +12,23 @@ type User struct {
 	Email        string         `json:"email" gorm:"uniqueIndex;not null;size:100"`
 	PasswordHash string         `json:"-" gorm:"not null;column:password_hash"`
 	SoftDelete   bool           `json:"soft_delete" gorm:"default:false;column:soft_delete"`
+	AuthLevel    string         `json:"auth_level" gorm:"default:user;column:auth_level"`
 	CreatedAt    time.Time      `json:"created_at"`
 	UpdatedAt    time.Time      `json:"updated_at"`
 	DeletedAt    gorm.DeletedAt `json:"deleted_at,omitempty" gorm:"index"`
 }
 
-
 type Profile struct {
-	ProfileID    uint       `json:"profile_id" gorm:"primaryKey;autoIncrement;column:profile_id"`
-	UserID       uint       `json:"user_id" gorm:"not null;index;column:user_id"`
-	FirstName    *string    `json:"first_name,omitempty" gorm:"size:50;column:first_name"`
-	LastName     *string    `json:"last_name,omitempty" gorm:"size:50;column:last_name"`
-	Gender       *string    `json:"gender,omitempty" gorm:"size:20"`
-	DateOfBirth  *time.Time `json:"date_of_birth,omitempty" gorm:"column:date_of_birth"`
-	Bio          *string    `json:"bio,omitempty" gorm:"type:text"`
-	City         *string    `json:"city,omitempty" gorm:"size:100"`
-	Country      *string    `json:"country,omitempty" gorm:"size:100"`
-	ValorantRank *string    `json:"valorant_rank,omitempty" gorm:"size:50;column:valorant_rank"`
-	CreatedAt    time.Time  `json:"created_at"`
-	UpdatedAt    time.Time  `json:"updated_at"`
+	ProfileID    uint              `json:"profile_id" gorm:"primaryKey;autoIncrement;column:profile_id"`
+	UserID       uint              `json:"user_id" gorm:"not null;index;column:user_id"`
+	FirstName    *string           `json:"first_name,omitempty" gorm:"size:50;column:first_name"`
+	LastName     *string           `json:"last_name,omitempty" gorm:"size:50;column:last_name"`
+	Gender       *string           `json:"gender,omitempty" gorm:"size:20"`
+	DateOfBirth  *time.Time        `json:"date_of_birth,omitempty" gorm:"column:date_of_birth"`
+	Bio          *string           `json:"bio,omitempty" gorm:"type:text"`
+	City         *string           `json:"city,omitempty" gorm:"size:100"`
+	Country      *string           `json:"country,omitempty" gorm:"size:100"`
+	GameRanks    map[string]string `json:"game_ranks,omitempty" gorm:"type:jsonb;column:game_ranks"`
+	CreatedAt    time.Time         `json:"created_at"`
+	UpdatedAt    time.Time         `json:"updated_at"`
 }
